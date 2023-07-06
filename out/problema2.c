58552c41f49bb0c96e572db8da1ef737124597b6
Raíssa Santos <raixapsantos@gmail.com>
2023-07-06 13:45:48 -0300

    doc: organização das pastas

---
+++ out/problema2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+int numero = 0;
+int intervalo1 = 0, intervalo2 = 0, intervalo3 = 0, intervalo4 = 0;
+label1:
+printf("Digite um numero negativo para encerrar: ");
+scanf("%i", numero);
+if(numero >= 0 and numero <= 25){
+intervalo1 += 1;
+}
+if(numero >= 26 and numero <= 50){
+intervalo2 += 1;
+}
+if(numero >= 51 and numero <= 75){
+intervalo3 += 1;
+}
+if(numero >= 76 and numero <= 100){
+intervalo4 += 1;
+}
+goto label1;
+fim:
+printf("Numeros no intervalo [0, 25]: \n", intervalo1);
+printf("Numeros no intervalo [26, 50]: \n", intervalo2);
+printf("Numeros no intervalo [51, 75]: \n", intervalo3);
+printf("Numeros no intervalo [76, 100]: \n", intervalo4);
+return 0;
+}
